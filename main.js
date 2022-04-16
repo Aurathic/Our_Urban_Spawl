2965c6c404cf8336f37c5c620d7e85f2918b08a2
Adam <39442291+Aurathic@users.noreply.github.com>
2022-04-16 16:39:26 -0400

    Set up bit of animation for main page

---
+++ main.js
@@ -0,0 +1,11 @@
+let links = document.querySelectorAll('.main_link');
+
+links.forEach(link => {
+	link.addEventListener("click", (event) => {
+    link.classList.add("move_left");
+    // After animation is done
+    link.addEventListener("transitionend", () => {
+    	window.location = link.getAttribute("href");
+  }, true);
+  })
+})
