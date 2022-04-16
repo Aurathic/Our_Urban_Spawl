68cb24a748ca2a5ee2cb661cc09ec5a7d3aaba92
Adam <39442291+Aurathic@users.noreply.github.com>
2022-04-16 17:15:07 -0400

    Fix error in JS load order
    JS now runs after HTML is loaded

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-let links = document.querySelectorAll('.main_link');
+window.onload = () => {
+    let links = document.querySelectorAll('.main_link');
 
-links.forEach(link => {
-	link.addEventListener("click", (event) => {
-    link.classList.add("move_left");
-    // After animation is done
-    link.addEventListener("transitionend", () => {
-    	window.location = link.getAttribute("href");
-  }, true);
-  })
-})
+    links.forEach(link => {
+        link.addEventListener("click", (event) => {
+        link.classList.add("move_left");
+        // After animation is done
+        link.addEventListener("transitionend", () => {
+            window.location = link.getAttribute("href");
+            }, true);
+        })
+    })
+}
