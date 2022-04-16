bb9747e21f1eb83c877ca79081cbf728d90ec804
Adam <39442291+Aurathic@users.noreply.github.com>
2022-04-16 17:38:02 -0400

    Fix issues with page after pressing back (from cached result)

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-window.onload = () => {
+window.onpageshow = () => {
     let links = document.querySelectorAll('.main_link');
-
     links.forEach(link => {
+        link.classList.remove("move_left");
         link.addEventListener("click", (event) => {
-        link.classList.add("move_left");
-        // After animation is done
-        link.addEventListener("transitionend", () => {
-            window.location = link.getAttribute("href");
-            }, true);
+            link.classList.add("move_left");
+            // Go to page after animation is done
+            link.addEventListener("transitionend", () => {
+                window.location = link.getAttribute("href");
+                }, { once: true });
         })
     })
 }
